08d75a696b627db08444f2bdafb72e59bf18f07c
alizduwal <duwal_amar2006@yahoo.com>
2019-06-13 18:49:40 +0545

    Setting up RSpec inside code

--- number_conversion.rb
+++ number_conversion.rb
@@ -33,6 +33,8 @@
 require 'pry'
 # To measure performance of code
 require 'benchmark'
+# For unit test
+require "rspec/autorun"
 
 class  NumberConversion
   attr_reader :phone_number, :wordlist
@@ -52,7 +54,6 @@ class  NumberConversion
     # Read wordlist file
     read_wordlist_file
     matching_words 
-    return @relevant_words.each { |x| puts x.join(", ") }
   end
 
   def not_valid_number?
@@ -76,7 +77,8 @@ class  NumberConversion
     breaking_into_three_part
 
     # For size of 10
-    @relevant_words << (@mapping_phone_number.shift.product(*@mapping_phone_number).map(&:join) & @wordlist[10])    
+    @relevant_words << (@mapping_phone_number.shift.product(*@mapping_phone_number).map(&:join) & @wordlist[10])
+    return @relevant_words
   end
 
   def breaking_into_two_part
@@ -148,10 +150,30 @@ class  NumberConversion
 
 end
 
-
 # Let's the method call from here
 print "Input 10 digits phone number without 0 & 1 : "
 phone_number = gets.chomp
 puts Benchmark.measure {
-  NumberConversion.new(phone_number).possible_word_combinations
+  print NumberConversion.new(phone_number).possible_word_combinations
 }
+
+
+# Not good at testing still let's give it a try
+describe NumberConversion do
+  phone_number1 = 6686787825
+  output1 = [["noun", "struck"], ["onto", "struck"], ["motor", "truck"], ["motor", "usual"], ["nouns", "truck"], ["nouns", "usual"], ["mot", "opt", "puck"], ["mot", "opt", "ruck"], ["mot", "opt", "suck"], ["mot", "ort", "puck"], ["mot", "ort", "ruck"], ["mot", "ort", "suck"], ["not", "opt", "puck"], ["not", "opt", "ruck"], ["not", "opt", "suck"], ["not", "ort", "puck"], ["not", "ort", "ruck"], ["not", "ort", "suck"], ["oot", "opt", "puck"], ["oot", "opt", "ruck"], ["oot", "opt", "suck"], ["oot", "ort", "puck"], ["oot", "ort", "ruck"], ["oot", "ort", "suck"], ["mot", "opts", "taj"], ["mot", "opus", "taj"], ["mot", "orts", "taj"], ["not", "opts", "taj"], ["not", "opus", "taj"], ["not", "orts", "taj"], ["oot", "opts", "taj"], ["oot", "opus", "taj"], ["oot", "orts", "taj"], ["noun", "pup", "taj"], ["noun", "pur", "taj"], ["noun", "pus", "taj"], ["noun", "sup", "taj"], ["noun", "suq", "taj"], ["onto", "pup", "taj"], ["onto", "pur", "taj"], ["onto", "pus", "taj"], ["onto", "sup", "taj"], ["onto", "suq", "taj"], ["motortruck"]]
+  
+  phone_number2 = 2282668687
+  output2 = [["act", "amounts"], ["act", "contour"], ["bat", "amounts"], ["bat", "contour"], ["cat", "amounts"], ["cat", "contour"], ["acta", "mounts"], ["act", "boo", "tots"], ["act", "boo", "tour"], ["act", "con", "tots"], ["act", "con", "tour"], ["act", "coo", "tots"], ["act", "coo", "tour"], ["bat", "boo", "tots"], ["bat", "boo", "tour"], ["bat", "con", "tots"], ["bat", "con", "tour"], ["bat", "coo", "tots"], ["bat", "coo", "tour"], ["cat", "boo", "tots"], ["cat", "boo", "tour"], ["cat", "con", "tots"], ["cat", "con", "tour"], ["cat", "coo", "tots"], ["cat", "coo", "tour"], ["act", "boot", "mus"], ["act", "boot", "nus"], ["act", "boot", "our"], ["act", "coot", "mus"], ["act", "coot", "nus"], ["act", "coot", "our"], ["bat", "boot", "mus"], ["bat", "boot", "nus"], ["bat", "boot", "our"], ["bat", "coot", "mus"], ["bat", "coot", "nus"], ["bat", "coot", "our"], ["cat", "boot", "mus"], ["cat", "boot", "nus"], ["cat", "boot", "our"], ["cat", "coot", "mus"], ["cat", "coot", "nus"], ["cat", "coot", "our"], ["acta", "mot", "mus"], ["acta", "mot", "nus"], ["acta", "mot", "our"], ["acta", "not", "mus"], ["acta", "not", "nus"], ["acta", "not", "our"], ["acta", "oot", "mus"], ["acta", "oot", "nus"], ["acta", "oot", "our"], ["catamounts"]]
+  context "Number to possible words conversion" do
+    it "Test combination of phone number with multiple words" do
+      expect(NumberConversion.new(phone_number1).possible_word_combinations).to  match_array(output1)
+    end
+  end
+
+  context "Number to possible words conversion" do
+    it "Test combination of phone number with multiple words" do
+      expect(NumberConversion.new(phone_number2).possible_word_combinations).to  match_array(output2)
+    end
+  end
+end
